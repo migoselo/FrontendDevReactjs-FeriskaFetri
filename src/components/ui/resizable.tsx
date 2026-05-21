ff77c7e386ff9e402d3f7b150fe55a34ec8ff655
migoselo <putridheerie@gmail.com>
2026-05-22 03:52:21 +0700

    update

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,46 +1,24 @@
 "use client"
 
 import { GripVertical } from "lucide-react"
-import * as ResizablePrimitive from "react-resizable-panels"
-import { Separator } from "react-resizable-panels"
 
-import { cn } from "@/lib/utils"
-
-const ResizablePanelGroup = ({
-  className,
-  ...props
-}: React.ComponentProps<any>) => (
-  <ResizablePrimitive.Group
-    className={cn(
-      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-      className
-    )}
-    {...props}
-  />
+// Placeholder components - version compatibility needed
+const ResizablePanelGroup = ({ children, ...props }: any) => (
+  <div className="flex h-full w-full" {...props}>{children}</div>
 )
 
-const ResizablePanel = ResizablePrimitive.Panel
+const ResizablePanel = ({ children, ...props }: any) => (
+  <div className="flex-1" {...props}>{children}</div>
+)
 
-const ResizableHandle = ({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof Separator> & {
-  withHandle?: boolean
-}) => (
-  <Separator
-    className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-      className
-    )}
-    {...props}
-  >
+const ResizableHandle = ({ withHandle }: any) => (
+  <div className="relative flex w-px items-center justify-center bg-border">
     {withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </Separator>
+  </div>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
